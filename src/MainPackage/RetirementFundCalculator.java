34a8bf0cc3383f281a6752738bac2ece39c4e3c9
Vinay <vvazir@udel.edu>
2016-02-18 19:44:37 -0500

    Added computations
    Need to add limits to annual returns, will prob do on Friday/Weekend

--- src/MainPackage/RetirementFundCalculator.java
+++ src/MainPackage/RetirementFundCalculator.java
@@ -1,6 +1,6 @@
 package MainPackage;
 import java.util.Scanner;
-
+import java.lang.Math.*;
 public class RetirementFundCalculator {
 
 	public static void main(String[] args) {
@@ -9,22 +9,29 @@ public class RetirementFundCalculator {
 		double yearsToWork;
 		double returnOnInvestment;
 		double yearsRetired;
-		double annualReturn=.02;
+		double annualReturn;
 		double requiredIncome;
+		double annualAfterRetirement;
 		double monthlySSI;
 		double moneyNeeded;
+		double monthlySaving;
 		System.out.println("How many years do you plan to work?");
 		yearsToWork=input.nextDouble();
-		System.out.println("What is your expected average return on investment?");
+		System.out.println("What is your expected average return on investment during this time?");
 		returnOnInvestment=input.nextDouble();
-		System.out.println("How long do you want to draw money for?");
+		System.out.println("How many years will you be in retirement?");
 		yearsRetired=input.nextDouble();
-		System.out.println("What is your required income?");
+		System.out.println("What is your expected annual return during retirement?");
+		annualReturn=input.nextDouble();
+		System.out.println("What is your required income during retirement?");
 		requiredIncome=input.nextDouble();
-		System.out.println("What is your expected SSI income?");
+		System.out.println("What is your expected monthly Social Security Income?");
 		monthlySSI=input.nextDouble();
-		moneyNeeded=requiredIncome-monthlySSI;
 		
+		moneyNeeded = (requiredIncome-monthlySSI)*((1-(1/(Math.pow(1+(annualReturn/100)/12,yearsRetired*12 )))))/((annualReturn/100)/12);
+	    monthlySaving = moneyNeeded*(((returnOnInvestment/100)/12)/((Math.pow(1+(returnOnInvestment/100)/12, yearsToWork*12))-1));
+	    System.out.printf("Total money needed: %.2f \n",moneyNeeded);
+	    System.out.printf("Monthly Payments: %.2f",monthlySaving);
 	}
 
 }
