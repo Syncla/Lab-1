1e60c55e25da6324125bc9bb2263af9c51610cf8
Vinay <vvazir@udel.edu>
2016-02-22 17:13:38 -0500

    Added do while loops and names
    Names added of all team members
    Do while loops insure that investment returns are between 0-20 and 0-3
    before and after retirement respectively.

--- src/MainPackage/RetirementFundCalculator.java
+++ src/MainPackage/RetirementFundCalculator.java
@@ -1,6 +1,12 @@
+/*Names of Lab Partners
+ * Vinay Vazir
+ * Alvin Tang
+ * Bonard Geyer
+ * Steven Lenkiewicz
+ * Raveena Wadhwa
+ */
 package MainPackage;
 import java.util.Scanner;
-import java.lang.Math.*;
 public class RetirementFundCalculator {
 
 	public static void main(String[] args) {
@@ -11,27 +17,36 @@ public class RetirementFundCalculator {
 		double yearsRetired;
 		double annualReturn;
 		double requiredIncome;
-		double annualAfterRetirement;
 		double monthlySSI;
 		double moneyNeeded;
 		double monthlySaving;
+		
 		System.out.println("How many years do you plan to work?");
 		yearsToWork=input.nextDouble();
-		System.out.println("What is your expected average return on investment during this time?");
+		
+		do{
+		System.out.println("What is your expected average return on investment during this time? (Enter a value from (0,20]");
 		returnOnInvestment=input.nextDouble();
+		} while (returnOnInvestment<=0 || returnOnInvestment>20);
+		
 		System.out.println("How many years will you be in retirement?");
 		yearsRetired=input.nextDouble();
-		System.out.println("What is your expected annual return during retirement?");
+		
+		do{
+		System.out.println("What is your expected annual return during retirement? (Enter a value from (0,3]");
 		annualReturn=input.nextDouble();
+		}while (annualReturn<=0 ||annualReturn>3);
+		
 		System.out.println("What is your required income during retirement?");
 		requiredIncome=input.nextDouble();
+		
 		System.out.println("What is your expected monthly Social Security Income?");
 		monthlySSI=input.nextDouble();
 		
 		moneyNeeded = (requiredIncome-monthlySSI)*((1-(1/(Math.pow(1+(annualReturn/100)/12,yearsRetired*12 )))))/((annualReturn/100)/12);
 	    monthlySaving = moneyNeeded*(((returnOnInvestment/100)/12)/((Math.pow(1+(returnOnInvestment/100)/12, yearsToWork*12))-1));
-	    System.out.printf("Total money needed: %.2f \n",moneyNeeded);
-	    System.out.printf("Monthly Payments: %.2f",monthlySaving);
+	    System.out.printf("Total money needed: $%.2f \n",moneyNeeded);
+	    System.out.printf("Monthly Payments: $%.2f",monthlySaving);
 	}
 
 }
